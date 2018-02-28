218d3ee96854b2fdf2bb6e1fbc0869ed9e9a30c7
Roxoli <przemyslaw.krzempek@o2.pl>
2018-02-28 17:56:00 +0100

    Poprawione logowanie

--- src/main/java/webapp/bankapp/BankappApplication.java
+++ src/main/java/webapp/bankapp/BankappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankappApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(BankappApplication.class, args);
 	}
 }
